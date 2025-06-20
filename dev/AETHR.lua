6091fd6adf6085f5124d4a5ea1cd05b16d4c7ea3
Gh0st352 <matthew.a.mirarchi@gmail.com>
2025-06-19 18:37:49 -0600

    Add storage structure and initialization functions to AETHR

--- dev/AETHR.lua
+++ dev/AETHR.lua
@@ -8,5 +8,29 @@ AETHR = {
         "It provides a set of tools and libraries to create dynamic and engaging scenarios.",
         "A high-fidelity simulation layer that weaves in adaptive machine learning decision-making across the whole theater.",
     },
+    STORAGE = {
     ROOT_FOLDER = "AETHR",
+    SUB_FOLDERS = {
+        CONFIG_FOLDER = "CONFIG",
+        TERRAIN_FOLDER = "TERRAIN",
+        UNITS_FOLDER = "UNITS",
+        OBJECTS_FOLDER = "OBJECTS",
+        USER_FOLDER = "USER",
+    },
+    },
 }
+
+function AETHR:New()
+    local instance = {}
+    setmetatable(instance, { __index = self })
+    return instance
+end
+
+function AETHR:Init()
+    for folderName, folderPath in pairs(self.STORAGE.SUB_FOLDERS) do
+        local fullPath = self.STORAGE.ROOT_FOLDER .. "/" .. folderPath
+        AETHR.fileOps.ensureDirectory(fullPath)
+    end
+
+    return self
+end
